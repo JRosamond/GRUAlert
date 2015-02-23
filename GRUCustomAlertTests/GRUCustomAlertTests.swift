8c1dd25afe7f340ae8d90511d293c3a530480529
Jennifer Rosamond <jrosamond@georgiahealth.edu>
2015-02-23 15:25:44 -0500

    commit additional test files.

---
+++ GRUCustomAlertTests/GRUCustomAlertTests.swift
@@ -0,0 +1,36 @@
+//
+//  GRUCustomAlertTests.swift
+//  GRUCustomAlertTests
+//
+//  Created by jrosamond on 2/2/15.
+//  Copyright (c) 2015 Georgia Regents University. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class GRUCustomAlertTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
